3a3466dab60f81ad1425891e525a8e667e51bc0e
Matlab Risk <intoverflown@localhost.localdomain>
2020-10-15 09:18:36 +0300

    Part 2: Create models, Register app and apply migrations

--- 4.blog-app2/blog/models.py
+++ 4.blog-app2/blog/models.py
@@ -11,7 +11,7 @@ class Post(models.Model):
     )
 
     title = models.CharField(max_length=250)
-    slag = models.SlugField(max_length=250, unique_for_date='publish')
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     
