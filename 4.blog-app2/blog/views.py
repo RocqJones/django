fb9fb5b5ef730d1ea05c0039be2c6cfa1199f439
Matlab Risk <intoverflown@localhost.localdomain>
2020-10-18 13:40:20 +0300

    Part 5: Build a list and detail view

--- 4.blog-app2/blog/views.py
+++ 4.blog-app2/blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+# create a list and a detailed view
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, 'list.html', {'posts': posts})
+
+def post_details(request, year, month, day, post):
+    post = get_object_or_404(Post, slug = post, status = 'published', 
+                            publish__year = 'year',
+                            publish__month = 'month',
+                            publish__day = 'day')
+
+    return render(request, 'detail.html', {'post': post})
